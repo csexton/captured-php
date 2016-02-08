87f1faab5579f1e8525e5c13eff391941d5045b3
Christopher Sexton <github@codeography.com>
2016-02-07 21:05:39 -0500

    Initial Pass at the Script
    Creates a script that uses a token to authenticate uploads and proxies
    the images back to the browser.
    
    The token is posted as part of the form. I did this because reading
    headers relies on a specific web server API to access (e.g. Apache). So
    it seemed like it would be more consistent to just use a form field.
    
    The UPLOAD_DIR does not have it's content served publicly like I
    originally planned. Instead the script will use `readfile` to proxy it
    back to the requester. This allows that directory to be located out of
    the website and prevents scripts from being run.
    
    Configuration can be handled by environment variables, but doesn't have to
    be. There is a way to modify this script to set the token and upload
    directory path.
    
    I used the `global` keyword a few times. Pretty sure this is a Bad
    Idea™, but it seemed like a good idea at the time. What could go wrong?

---
+++ captured.php
@@ -0,0 +1,109 @@
+<?php
+////////////////////////////////////////////////////////////////////////////////
+//
+// Captured PHP Upload Script
+//
+// This is a simple script that can provide a target for Captured App to upload
+// to any webserver that supports PHP. For more details or to get Captured
+// visit:
+//
+// http://www.capturedapp.com/
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// Configuration Options
+//
+// The folling options can either be set with the corresponding envioment
+// variables or by manually changing the options in the script below.
+//
+// To set the envioment variables in a `.htaccess` file you can do the following:
+//
+// SetEnv CAPTURED_TOKEN change_this_token
+// SetEnv CAPTURED_UPLOAD_DIR /home/user/website.com/upload-dir/
+//
+////////////////////////////////////////////////////////////////////////////////
+//
+// API Token used to authenticate requests. Do not use the default.
+//
+$API_TOKEN=$_ENV["CAPTURED_API_TOKEN"] ?: "change_this_token";
+//
+// Upload Directory. Needs to be writable by PHP on this server. Does not have
+// to be publically served, since this script will proxy images back.
+//
+$UPLOAD_DIR = $_ENV["CAPTURED_UPLOAD_DIR"] ?: "./uploads/";
+//
+////////////////////////////////////////////////////////////////////////////////
+
+function jecho($arr) {
+  echo json_encode($arr, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+}
+
+function join_paths() {
+  $paths = array();
+  foreach (func_get_args() as $arg) {
+    if ($arg !== '') { $paths[] = $arg; }
+  }
+  return preg_replace('#/+#','/',join('/', $paths));
+}
+
+function randomish($length = 8) {
+  $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  $string = '';
+
+  for ($i = 0; $i < $length; $i++) {
+    $string .= $characters[mt_rand(0, strlen($characters) - 1)];
+  }
+  return $string;
+}
+
+function enforce_token() {
+  global $API_TOKEN;
+  if($_POST['token'] != $API_TOKEN) {
+    print_r($_POST);
+    http_response_code(401);
+    jecho(['error' => "Invalid Token"]);
+    exit();
+  }
+}
+
+function render_image() {
+  global $UPLOAD_DIR;
+  $file = join_paths($UPLOAD_DIR, basename($_GET['i']));
+  if (file_exists($file)) {
+    header("Content-Type: image/" . pathinfo($file, PATHINFO_EXTENSION));
+    readfile($file);
+    exit();
+  } else {
+    http_response_code(404);
+    jecho(['error' => "File Not Found"]);
+  }
+}
+
+function process_image_upload() {
+  global $UPLOAD_DIR;
+
+  if(!is_dir($UPLOAD_DIR)) { mkdir($UPLOAD_DIR); }
+
+  $protocol= (empty($_SERVER['HTTPS'])) ? 'http://' : 'https://';
+  $ext = pathinfo($_FILES['file']['name'], PATHINFO_EXTENSION);
+  $upload_name = randomish() . '.' . $ext;
+  $upload_file = join_paths($UPLOAD_DIR, $upload_name);
+  $public_url = $protocol . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'] . "/?i=" . $upload_name;
+
+  header('Content-Type: application/json');
+
+  if (move_uploaded_file($_FILES['file']['tmp_name'], $upload_file)) {
+    jecho(["public_url" => $public_url]);
+  } else {
+    jecho(['error' => "Unable to process image upload"]);
+  }
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  enforce_token();
+  process_image_upload();
+} else {
+  render_image();
+}
+
+?>
